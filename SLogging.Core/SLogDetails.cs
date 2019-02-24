a62ef13331e7d33b6b46362b75b7c43f7f43dece
Rosando Desouza <desouza.rosando@gmail.com>
2019-02-24 13:05:30 +0530

    Added logdetails properties and logger to perform the file based logging

--- SLogging.Core/SLogDetails.cs
+++ SLogging.Core/SLogDetails.cs
@@ -1,6 +1,44 @@
-﻿namespace SLogging.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace SLogging.Core
 {
     public class SLogDetails
     {
+        public SLogDetails()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+
+        public DateTime Timestamp { get; }
+
+        public string Message { get; set; }
+
+        //Where
+        public string Product { get; set; }
+
+        public string Layer { get; set; }
+
+        public string Location { get; set; }
+
+        public string HostName { get; set; }
+
+        //Who
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        //Everything Else
+        public long? ElapsedMilliseconds { get; set; } //Only for performance entries
+
+        public Exception Exception { get; set; } //The exception for error logging
+
+        public string CorrelationId { get; set; } //Exception shielding from server to client
+
+        public Dictionary<string, object> AdditionalInfo { get; set; } //Catch-All for anything else
     }
 }
---
+++ SLogging.Core/SLogger.cs
@@ -0,0 +1,52 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace SLogging.Core
+{
+    public static class SLogger
+    {
+        private static readonly ILogger _performanceLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+
+        static SLogger()
+        {
+            _performanceLogger = new LoggerConfiguration()
+                .WriteTo.Async(x => x.File(path: "perf.txt"))
+                .CreateLogger();
+
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.Async(x => x.File(path: "usage.txt"))
+                .CreateLogger();
+
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.Async(x => x.File(path: "error.txt"))
+                .CreateLogger();
+
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.Async(x => x.File(path: "diagnostic.txt"))
+                .CreateLogger();
+        }
+
+        public static void WritePerformance(SLogDetails performanceLogDetails)
+        {
+            _performanceLogger.Write(LogEventLevel.Information, "{@SLogDetails}", performanceLogDetails);
+        }
+
+        public static void WriteUsage(SLogDetails usageLogDetails)
+        {
+            _usageLogger.Write(LogEventLevel.Information, "{@SLogDetails}", usageLogDetails);
+        }
+
+        public static void WriteError(SLogDetails errorLogDetails)
+        {
+            _errorLogger.Write(LogEventLevel.Error, "{@SLogDetails}", errorLogDetails);
+        }
+
+        public static void WriteDiagnostic(SLogDetails duagnosticLogDetails)
+        {
+            _diagnosticLogger.Write(LogEventLevel.Information, "{@SLogDetails}", duagnosticLogDetails);
+        }
+    }
+}
